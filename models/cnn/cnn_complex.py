1a12ad917a7914150251fd00dbf3f6188393f824
charan-435 <charanm435@gmail.com>
2026-04-10 18:09:51 +0530

    Trained on large data

--- models/cnn/cnn_complex.py
+++ models/cnn/cnn_complex.py
@@ -8,6 +8,7 @@ class CnnComplex:
     def __init__(self, embed_dim: int, sequence_length: int):
         self.embed_dim       = embed_dim
         self.sequence_length = sequence_length
+        
 
     def build(self, char_index: dict) -> Sequential:
         voc_size=len(char_index)
